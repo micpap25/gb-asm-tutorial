fcf8260124b4b51d4745fef024460de4270be880
micpap25 <micpap25@gmail.com>
2021-06-29 09:54:59 -0400

    Did some work, waiting for book to be updated to continue

---
+++ Basics (Outdated)/syntax.asm
@@ -0,0 +1,7 @@
+
+
+di ; An instruction that takes no operands
+
+cp a, $11 ; Compare value left by boot ROM, an instruction that takes TWO operands
+
+jr .local ; An instruction that takes one OR two operands (in this case, it is taking one)
