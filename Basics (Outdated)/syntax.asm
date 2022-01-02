be69d8fae3ccc13d7f1b1ddcdb0318a0f70ea723
micpap25 <micpap25@gmail.com>
2022-01-02 10:49:48 -0500

    Starting from Scratch

--- Basics (Outdated)/syntax.asm
+++
@@ -1,7 +0,0 @@
-
-
-di ; An instruction that takes no operands
-
-cp a, $11 ; Compare value left by boot ROM, an instruction that takes TWO operands
-
-jr .local ; An instruction that takes one OR two operands (in this case, it is taking one)
